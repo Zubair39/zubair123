af4fa705d4e990109176941aef2b49f4dd4b24bc
Mohamed zubair <51960918+Zubair39@users.noreply.github.com>
2019-06-28 12:27:39 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,8 @@
+a=str(input())
+if (a.isalpha()):
+    if(a=='a' or a=='e' or a=='i' or a=='o' or a=='u'):
+        print('Vowel')
+    else:
+        print('Consonant')
+else:
+    print('invalid')
