29fe0476bb7eba734665b37be2478daf16386e6a
Mohamed zubair <51960918+Zubair39@users.noreply.github.com>
2019-07-16 01:56:23 -0700

    Create pro52.py

---
+++ pro52.py
@@ -0,0 +1,23 @@
+z1=int(input())
+if z1==1:
+    print("One")
+elif z1==2:
+    print("Two")
+elif z1==3:
+    print("Three")
+elif z1==4:
+    print("Four")
+elif z1==5:
+    print("Five")
+elif z1==6:
+    print("Six")
+elif z1==7:
+    print("seven")
+elif z1==8:
+    print("Eight")
+elif z1==9:
+    print("Nine")
+elif z1==10:
+    print("Ten")
+else:
+    print("no")
