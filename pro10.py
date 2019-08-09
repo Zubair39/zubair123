616e21ff426c506f388f48899dce2d29bda64ace
Mohamed zubair <51960918+Zubair39@users.noreply.github.com>
2019-08-09 12:10:09 +0530

    Update pro10.py

--- pro10.py
+++ pro10.py
@@ -1,7 +1,7 @@
 chaa1=int(input())
 choa=[int(o) for o in input().split(" ")]
 chea=0
-for p in range(chaa):
+for p in range(chaa1):
       for g in range(p):
            if(choa[g]<choa[p]):
                 chea+=choa[g]
