ccce85b05c0bcbe84c41a1306bc9ec7ac3569c3d
Mohamed zubair <51960918+Zubair39@users.noreply.github.com>
2019-08-09 11:36:30 +0530

    Update pro3.py

--- pro3.py
+++ pro3.py
@@ -1,8 +1,8 @@
-a,e=input().split()
-char=abs(len(ee)-len(a))
-for g in range(len(a)):
-  if(len(ee)==1 and ee[e] in a):
+a1,b=input().split()
+char=abs(len(b)-len(a1))
+for g in range(len(a1)):
+  if(len(b)==1 and b[g] in a1):
     break
-  if(a[g]!=ee[g]):
+  if(a1[g]!=b[g]):
     char=char+1
 print(char)
