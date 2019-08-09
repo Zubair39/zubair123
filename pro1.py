023ea4fda06b3368fc4c91832b8410f17454cacc
Mohamed zubair <51960918+Zubair39@users.noreply.github.com>
2019-08-09 11:24:33 +0530

    Create pro1.py

---
+++ pro1.py
@@ -0,0 +1,12 @@
+hi=int(input())
+df=[]
+for i in range(0,hi):
+ ij=input()
+ df.append(ij)
+ff=[]
+for i in zip(*df):
+ if(i.count(i[0])==len(i)):
+  ff.append(i[0])
+ else:
+  break
+print(''.join(ff))
