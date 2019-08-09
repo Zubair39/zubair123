1bf246f5b8e269761ca31b579f6da1585913fa1d
Mohamed zubair <51960918+Zubair39@users.noreply.github.com>
2019-08-09 11:57:51 +0530

    Update proo4.py

--- proo4.py
+++ proo4.py
@@ -1,11 +1,11 @@
-c1,c2=map(str,input().split())
-yes=0
-if len(c1)>len(c2):
-  c1,c2=c2,c1
+c11,c22=map(str,input().split())
+yas=0
+if len(c11)>len(c22):
+  c11,c22=c22,c11
 p=0
-while p<len(c1):
-  yes+=(erd(c2[p])-erd(c1[p]))
+while p<len(c11):
+  yas+=(ord(c22[p])-ord(c11[p]))
   p+=1
-for p in range(p,len(c2)):
-  yes+=erd(c2[p])-erd('a')+1
-print(yes)
+for p in range(p,len(c22)):
+  yas+=ord(c22[p])-ord('a')+1
+print(yas)
