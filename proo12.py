b5200bac95ae014f2b8e5a4f6f52e47cbeb60daf
Mohamed zubair <51960918+Zubair39@users.noreply.github.com>
2019-08-11 16:56:35 +0530

    Create proo12.py

---
+++ proo12.py
@@ -0,0 +1,8 @@
+import sys, string, math
+n1,k1 = input().split()
+n1,k1 = int(n1), int(k1)
+L = [ int(x) for x in input().split()]
+for i in range(0,k) :
+    a,b = input().split()
+    a,b = int(a), int(b)
+    print(sum(L[a-1:b]))
