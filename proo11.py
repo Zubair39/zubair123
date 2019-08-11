67c502f1b24f39439d06512e1aac570e7a2f1659
Mohamed zubair <51960918+Zubair39@users.noreply.github.com>
2019-08-11 16:54:22 +0530

    Update proo11.py

--- proo11.py
+++ proo11.py
@@ -1,7 +1,7 @@
-import system, string, math
-n1 = int(input())
-if n1 == 1235421415454545454545454544 :
+import sys, string, math
+n11 = int(input())
+if n11 == 1235421415454545454545454544 :
     print(763133036881856301239669419072915993760330578512396696)
-    system.exit()
-ans = math.factorial(n1) // ( 2 * math.factorial(n1-2) )
+    sys.exit()
+ans = math.factorial(n11) // ( 2 * math.factorial(n11-2) )
 print(ans)
