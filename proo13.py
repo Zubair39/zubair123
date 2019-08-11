edf07cd7e0b9abccd0b33601d811e0fadcbabbe9
Mohamed zubair <51960918+Zubair39@users.noreply.github.com>
2019-08-11 17:03:47 +0530

    Update proo13.py

--- proo13.py
+++ proo13.py
@@ -3,7 +3,7 @@ import sys, string, math
 n11,k11 = input().split()
 n11,k11 = int(n11), int(k11)
 L = [ int(x) for x in input().split()]
-for i in range(0,k) :
+for i in range(0,k11) :
     a,b = input().split()
     a,b = int(a), int(b)
     print(min(L[a-1:b]))
