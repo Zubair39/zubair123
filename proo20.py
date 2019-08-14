38da2d0874f8547f9a9f718c659b1a44923d8f76
Mohamed zubair <51960918+Zubair39@users.noreply.github.com>
2019-08-14 12:08:02 +0530

    Create proo20.py

---
+++ proo20.py
@@ -0,0 +1,17 @@
+#a
+x1,y1=map(int,input().split())
+arr=list(map(int,input().split()))
+arr.sort(reverse=True)
+a=0
+total=y1
+for i in arr:
+    if total>=i:
+        rem=int(total/i)
+        a+=rem
+        total=total - (i*rem)
+    if total==0:
+        break
+if total==0:
+    print(a)
+else:
+    print("it's not possible to sum up coins in such a way that they um upto",y1)
